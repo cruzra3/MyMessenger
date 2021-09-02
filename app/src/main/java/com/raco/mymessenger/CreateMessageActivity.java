48ffb9bce800611776716f5452a620798aec02cf
Rafael Angel Cruz <rcruz315@yahoo.com>
2021-09-02 10:13:42 -0400

    1 Tweak the layout activity_receive_message.xml so that it can display the text. At the moment it’s simply the default layout the wizard gave us.
    2 Update CreateMessageActivity.java so that it gets the text the user inputs, and then adds the text to the intent before it sends it.
    
    3 Update ReceiveMessageActivity.java so that it displays the text sent in the intent.

--- app/src/main/java/com/raco/mymessenger/CreateMessageActivity.java
+++ app/src/main/java/com/raco/mymessenger/CreateMessageActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends AppCompatActivity {
 
@@ -15,7 +16,10 @@ public class CreateMessageActivity extends AppCompatActivity {
     }
 
     public void onSendMessage(View view) {
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
         Intent intent = new Intent(this, ReceiveMessageActivity.class);
-        startActivity(intent); //
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+        startActivity(intent);
     }
 }
